c36b58196a363d7342dee31bccbf82a1adf9f9fa
Michael Chambers <michael.chambers@altum.com>
2020-04-21 12:04:51 -0400

    Typing

---
+++ index.d.ts
@@ -0,0 +1,38 @@
+interface ReadabilityConfig {
+	difficultWords ?:boolean;
+	capsAsNames ?:boolean;
+	onlySpache ?:boolean;
+	onlyDaleChall ?:boolean;
+	onlyARI ?:boolean;
+	onlyColemanLiau ?:boolean;
+	onlyFleschKincaid ?:boolean;
+	onlySMOG ?:boolean;
+	onlyGunningFog ?:boolean;
+	skipDaleChall ?:boolean;
+	skipARI ?:boolean;
+	skipColemanLiau ?:boolean;
+	skipFleschKincaid ?:boolean;
+	skipSMOG ?:boolean;
+	skipGunningFog ?:boolean;
+}
+
+function readabilityScores(value :string, config ?:ReadabilityConfig) :{
+	letterCount :number;
+	syllableCount :number;
+	wordCount :number;
+	sentenceCount :number;
+	polysyllabicWordCount :number;
+	polysyllabicWords ?:string[];
+	spacheUniqueUnfamiliarWordCount ?:number;
+	spacheUniqueUnfamiliarWords ?:string[];
+	spache ?:number;
+	daleChallDifficultWordCount ?:number;
+	daleChallDifficultWords ?:string[];
+	daleChall ?:number;
+	ari ?:number;
+	colemanLiau ?:number;
+	fleschKincaid ?:number;
+	smog ?:number;
+	gunningFog ?:number;
+}
+export = readabilityScores
